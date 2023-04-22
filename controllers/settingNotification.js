c4e14023cacd1f2e0bd1cf1ce9915adb15e55828
wichuda.ph.62 <wichuda.ph.62@ubu.ac.th>
2023-04-22 20:55:43 +0700

    setting notification log

--- controllers/settingNotification.js
+++ controllers/settingNotification.js
@@ -1,5 +1,6 @@
 const SettingNotificationService = require('../services/settingNotificationService');
 const { check, validationResult } = require('express-validator');
+const {logger} = require('../config/settingNotificationLogger');
 
 const validate = (method) => {
   switch (method) {
@@ -7,9 +8,9 @@ const validate = (method) => {
       return [
         check('custom_key', 'custom_key is required').not().isEmpty(),
         check('text', 'text is required').not().isEmpty(),
-        check('email_notification', 'email_notification is required').not().isEmpty(),
-        check('line_notification', 'line_notification is required').not().isEmpty(),
-        check('iot_notification', 'iot_notification is required').not().isEmpty(),
+        // check('email_notification', 'email_notification is required').not().isEmpty(),
+        // check('line_notification', 'line_notification is required').not().isEmpty(),
+        // check('iot_notification', 'iot_notification is required').not().isEmpty(),
       ];
     }
     default:
@@ -20,8 +21,10 @@ const validate = (method) => {
 const getAllSettingNotifications = async (req, res, next) => {
   try {
     const settingNotifications = await SettingNotificationService.getAllSettingNotifications();
+    logger.info(`Method(GET), Get all setting notifications data.`);
     res.status(200).json(settingNotifications);
   } catch (error) {
+    logger.error(error);
     next(error);
   }
 };
@@ -30,10 +33,13 @@ const getSettingNotificationById = async (req, res, next) => {
   try {
     const settingNotification = await SettingNotificationService.getSettingNotificationById(req.params.id);
     if (!settingNotification) {
+      logger.error(`Method(GET), Setting notification with ID ${req.params.id} not found.`);
       return res.status(204).json({ message: 'Setting notification data not found' });
     }
+    logger.info(`Method(GET), Get setting notification with ID ${req.params.id}.`);
     res.status(200).json(settingNotification);
   } catch (error) {
+    logger.error(error);
     next(error);
   }
 };
@@ -46,8 +52,10 @@ const createSettingNotification = async (req, res, next) => {
     }
 
     const newSettingNotification = await SettingNotificationService.createSettingNotification(req.body);
+    logger.info(`Method(POST), Create setting notification successfully.`);
     res.status(201).json(newSettingNotification);
   } catch (error) {
+    logger.error(error);
     next(error);
   }
 };
@@ -56,12 +64,15 @@ const updateSettingNotificationById = async (req, res, next) => {
   try {
     const notification = await SettingNotificationService.getSettingNotificationById(req.params.id);
     if (!notification) {
+      logger.error(`Method(PUT), Setting notification with ID ${req.params.id} not found.`);
       return res.status(204).json({ message: 'Setting notification not found' });
     }
 
     const updatedSettingNotification = await SettingNotificationService.updateSettingNotificationById(req.params.id, req.body);
+    logger.info(`Method(PUT), Update setting notification with ID ${req.params.id}.`);
     res.status(200).json(updatedSettingNotification);
   } catch (error) {
+    logger.error(error);
     next(error);
   }
 };
@@ -70,10 +81,12 @@ const deleteSettingNotificationById = async (req, res, next) => {
   try {
     const notification = await SettingNotificationService.getSettingNotificationById(req.params.id);
     if (!notification) {
+      logger.error(`Method(DELETE), Setting notification with ID ${req.params.id} not found.`);
       return res.status(204).json({ message: 'Setting notification not found' });
     }
 
     const deletedSettingNotification = await SettingNotificationService.deleteSettingNotificationById(req.params.id);
+    logger.info(`Method(DELETE), Delete setting notification with ID ${req.params.id}.`);
     res.status(204).json(deletedSettingNotification);
   } catch (error) {
     next(error);
