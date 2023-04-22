94c76c9144a5167e7656d0983d240f7de14dfaf8
wichuda.ph.62 <wichuda.ph.62@ubu.ac.th>
2023-04-22 20:38:51 +0700

    alarm sound log

--- controllers/alarmSound.js
+++ controllers/alarmSound.js
@@ -1,16 +1,19 @@
 const { check, validationResult } = require('express-validator');
 const AlarmSoundService = require('../services/alarmSoundService');
+const {logger} = require('../config/soundLogger');
 
 const validate = () => {
   return [
     check('fileName').custom((value, { req }) => {
       if (!req.file) {
+        logger.error(`Method(POST), Alarm file is required.`);
         throw new Error('File is required');
       }
       return true;
     }),
     check('sound_name').custom((value, { req }) => {
       if (!value) {
+        logger.error(`Method(POST), Alarm sound name is required.`);
         throw new Error('Sound name is required');
       }
       return true;
@@ -22,6 +25,7 @@ const uploadAlarmSound = async (req, res, next) => {
   try {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      // logger.error(errors);
       return res.status(400).json({ errors: errors.array() });
     }
 
@@ -32,8 +36,10 @@ const uploadAlarmSound = async (req, res, next) => {
       sound_name: req.body.sound_name
     };
     const newAlarmSound = await AlarmSoundService.createAlarmSound(alarmSoundData);
+    logger.info(`Method(POST), Upload alarm file successfully.`);
     res.status(201).json(newAlarmSound);
   } catch (error) {
+    logger.error(error);
     next(error);
   }
 };
@@ -42,6 +48,7 @@ const updateAlarmSound = async (req, res, next) => {
   try {
     const alarmSound = await AlarmSoundService.getAlarmSoundById(req.params.id);
     if (!alarmSound) {
+      logger.error(`Method(PUT), Alarm sound with ID ${req.params.id} not found.`);
       return res.status(204).json({ message: 'Alarm sound with the given id not found' });
     }
 
@@ -59,8 +66,10 @@ const updateAlarmSound = async (req, res, next) => {
     } 
     
     const updatedAlarmSound = await AlarmSoundService.updateAlarmSoundById(req.params.id, newAlarmSoundData);
+    logger.info(`Method(PUT), Update alarm sound with ID ${req.params.id}.`);
     res.status(200).json(updatedAlarmSound);
   } catch (error) {
+    logger.error(error);
     next(error);
   }
 };
@@ -78,8 +87,10 @@ const getAllAlarm = async (req, res, next) => {
         updated_at: sound.updated_at,
       };
     });
+    logger.info(`Method(GET), Get all alarm sound.`);
     res.status(200).json(result);
   } catch (error) {
+    logger.error(error);
     next(error);
   }
 };
@@ -89,6 +100,7 @@ const getAlarmById = async (req, res) => {
   try {
     const result = await AlarmSoundService.getAlarmSoundById(id);
     if (!result || !result.file) {
+      logger.error(`Method(GET), Alarm sound with ID ${req.params.id} not found.`);
       console.log(`No audio file found for id: ${id}`);
       return res.status(204).send();
     }
@@ -101,8 +113,10 @@ const getAlarmById = async (req, res) => {
 
     res.write(file);
     res.end();
+    logger.info(`Method(GET), Get alarm sound with ID ${req.params.id}.`);
   } catch (err) {
     console.log(err);
+    logger.error(err);
     return res.status(500).json({ message: err });
   }
 };
@@ -111,12 +125,15 @@ const deleteAlarmSound = async (req, res, next) => {
   try {
     const alarmSound = await AlarmSoundService.getAlarmSoundById(req.params.id);
     if (!alarmSound) {
+      logger.error(`Method(DELETE), Alarm sound with ID ${req.params.id} not found.`);
       return res.status(204).json({ message: 'Alarm sound not found' });
     }
 
     const deletedAlarmSound = await AlarmSoundService.deleteAlarmSoundById(req.params.id);
+    logger.info(`Method(DELETE), Deleted alarm sound with ID ${req.params.id}.`);
     res.status(204).json(deletedAlarmSound);
   } catch (error) {
+    logger.error(error);
     next(error);
   }
 };
